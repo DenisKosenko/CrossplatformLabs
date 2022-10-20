8cfa9ee86bb12258c2b1aa4bc810530eee0717e7
DenisKosenko <56043591+DenisKosenko@users.noreply.github.com>
2022-10-21 00:25:15 +0300

    CS-1: done lab 1

---
+++ Lab1_6/Lab1_6/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab1_6
+{
+    public class Program
+    {
+        public static string InputFilePath = @"input.txt";
+        public static string OutputFilePath = @"output.txt";
+
+        static void Main(string[] args)
+        {
+            FileInfo outputFileInfo = new FileInfo(OutputFilePath);
+            int inputNumber = Convert.ToInt32(File.ReadLines(InputFilePath).First().Trim());
+            if (inputNumber >= 1 && inputNumber <= 109)
+            {
+                int result = 0;
+                for(int i = 1; i <= inputNumber; i += 2)
+                {
+                    if (i % 3 != 0 && i % 5 != 0)
+                    {
+                        result++;
+                    }
+                }
+
+                using (StreamWriter streamWriter = outputFileInfo.CreateText())
+                {
+                    streamWriter.WriteLine(result);
+                }
+            }
+            else
+            {
+                using (StreamWriter streamWriter = outputFileInfo.CreateText())
+                {
+                    streamWriter.WriteLine("Number is out of range");
+                }
+            }
+        }
+    }
+}
